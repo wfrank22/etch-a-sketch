b583ead9fedef1479635b2a3d4d79b9a75e67a7b
wfrank22 <walkerfrank22@gmail.com>
2022-07-25 22:28:16 -0700

    Add mouseOver

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const grid = document.getElementById('grid');
-const gridSquare = document.createElement('div');
+// const gridSquareRow = document.createElement('div');
+// const gridSquareColumn = document.createElement('div');
 
 function createGrid(gridSize){
     for(let i = 0; i < gridSize; i++){ //create rows
         let row = document.createElement('div');
         grid.appendChild(row).className = 'gridSquareRow';
-        row.innerHTML = 'Row';
         for(let j = 0; j < gridSize-1; j++){ //create columns
             let column = document.createElement('div');
             grid.appendChild(column).className = 'gridSquareColumn';
-            column.innerHTML = 'Column';
         }
     }
 }
 
-
 createGrid(16);
+
+document.addEventListener('mouseover', function(e){
+    if(e.target.classList.contains('gridSquareRow' || 'gridSquareColumn')){
+        mouseOver();
+    }
+})
+
+function mouseOver(){
+    document.grid.style.backgroundColor = 'black';
+    document.gridSquareColumn.style.backgroundColor = 'black';
+}
